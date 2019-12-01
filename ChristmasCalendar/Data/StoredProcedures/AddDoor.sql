7ec2f4a8b885c6dc922e43adb4cec40eb6393f06
Hallstein Brøtan <hbrotan@gmail.com>
2019-12-01 22:06:07 +0100

    fix(sln): img to wide on mobile++

---
+++ ChristmasCalendar/Data/StoredProcedures/AddDoor.sql
@@ -0,0 +1,24 @@
+﻿DECLARE @guid UNIQUEIDENTIFIER
+SET @guid = NEWID()
+
+INSERT INTO [dbo].[Door]
+           ([Country]
+           ,[Location]
+           ,[ImagePath]
+           ,[ForDate]
+           ,[Number]
+           ,[Description]
+           ,[PointsForCountry]
+           ,[PointsForLocation])
+     VALUES
+           ('Sverige'
+           ,'Nord-Koster'
+           ,'https://julekalender.blob.core.windows.net/2019/' + cast(@guid as varchar(100)) +'.jpg'
+           ,'2019-12-20'
+           ,15
+           ,'Kategori: Vanskelig'
+           ,1
+           ,3)
+
+
+SELECT @guid
